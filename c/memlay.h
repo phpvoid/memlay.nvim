b7781fdd6562ab299a6aa84640860d88c50a8035
phpvoid <ppopko03@gmail.com>
2026-05-18 22:04:43 -0500

    Update memlay.h

--- c/memlay.h
+++ c/memlay.h
@@ -7,8 +7,8 @@
 #define MAX_NAME   128
 
 typedef struct {
-    char   name[MAX_NAME];
-    char   type_name[64];
+    char name[MAX_NAME];
+    char type_name[64];
     size_t size;
     size_t align;
     size_t offset;
@@ -17,13 +17,13 @@ typedef struct {
 
 typedef struct {
     FieldInfo fields[MAX_FIELDS];
-    int       field_count;
-    size_t    total_size;
-    size_t    packed_size;
-    char      suggestion[1024];
-    char      struct_name[128];
-    int       struct_start_line;
-    int       struct_end_line;
+    int field_count;
+    size_t total_size;
+    size_t packed_size;
+    char suggestion[1024];
+    char struct_name[128];
+    int struct_start_line;
+    int struct_end_line;
 } LayoutResult;
 
 void compute_layout(FieldInfo *fields, int count, LayoutResult *out);
